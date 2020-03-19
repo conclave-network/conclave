b117ab322f0cd4198cd220c06d5f4fe8e5b62a7b
Noel O'Donnell <6617714+odonnellnoel@users.noreply.github.com>
2020-03-19 23:13:29 +0000

    initial boilerplate for ecdsa signature

---
+++ src/ecdsa_signature.h
@@ -0,0 +1,41 @@
+/*
+ * CONCLAVE - Making Bitcoin Scale And Be Useful
+ * Copyright (C) 2019-2020 Noel P. O'Donnell <noel.odonnell.2020@mumail.ie>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "hash256.h"
+
+namespace conclave
+{
+    /***
+     * Representation of DER-encoded ECDSA signature with sighash byte
+     */
+    class EcdsaSignature final
+    {
+        public:
+        // Constructors
+        // Public Functions
+        // Conversions
+        // Operator Overloads
+        private:
+        // Properties
+        Hash256 r;
+        Hash256 s;
+        BYTE sighash;
+    };
+}
